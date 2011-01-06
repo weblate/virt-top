2f3a068823c1ff8dcf6ddd5295a6fc8b10ca8756
Richard W.M. Jones <rjones@redhat.com>
2011-01-06 12:40:29 +0000

    main: Record and print full exception stack traces.
    This turns on stack trace recording unconditionally (the same
    effect as if OCAMLRUNPARAM=b was always supplied), and also prints
    stack traces to the log file if there is an exception.
    
    See also:
    http://caml.inria.fr/pub/docs/manual-ocaml/libref/Printexc.html
    
    Note that this requires OCaml >= 3.11.0.

--- virt-top/virt_top_main.ml
+++ virt-top/virt_top_main.ml
@@ -34,6 +34,7 @@ let error =
   let ((_, _, script_mode, _, _, _, _) as setup) = start_up () in
 
   try
+    Printexc.record_backtrace true;
     main_loop setup;
     if not script_mode then endwin ();
     false
@@ -41,10 +42,12 @@ let error =
   | Libvirt.Virterror err ->
       if not script_mode then endwin ();
       prerr_endline (Libvirt.Virterror.to_string err);
+      Printexc.print_backtrace stderr;
       true
   | exn ->
       if not script_mode then endwin ();
       prerr_endline (s_ "Error" ^ ": " ^ Printexc.to_string exn);
+      Printexc.print_backtrace stderr;
       true
 
 let () =
